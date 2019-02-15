bb85e7af3239e8bb12f09c61bb34a318851f2904
tuckermullen <mullentucker@gmail.com>
2019-02-15 15:25:43 -0800

    Week 0 assessment in progress, not complete

--- WEEK-0.js
+++ WEEK-0.js
@@ -3,18 +3,18 @@
 var mantra = "Be the dev";
 
 // 1. Write the code that returns 'B' from mantra.
-
+console.log(mantra[0])
 // 2. Write the code that determines if there is a 'x' in mantra.
-
+console.log(mantra.includes("x"))
 // 3. Write the code that determines if there is a 'v' in mantra.
-
+console.log(mantra.includes("v"))
 // Stretch: Write the code that returns the position of 'v' in mantra.
-
+console.log(mantra.indexOf("v"))
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
+var codingIsEasy = true
+var imBored = true
 
 if (codingIsEasy && imBored) {
     console.log("I need more!")
@@ -26,9 +26,10 @@ if (codingIsEasy && imBored) {
 
 // Consider the following statement:
 
-var imLost
-var imFrustrated
-
+var imLost = true
+var imFrustrated = true
+// You can assign any sort of data to these variables to make them true to return 'Break time!' but
+// I decided to assign a value to both for the sake of the assessment.
 if (imLost || imFrustrated) {
     console.log('Break time!')
 } else {
@@ -38,16 +39,49 @@ if (imLost || imFrustrated) {
 // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
 
 // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
-
+for (let i = 0; i < mantra.length; i++){
+    console.log(mantra[i]);
+}
 // Stretch: Using a while loop.
-
+var i = 0
+while (i < mantra.length) {
+  console.log(mantra[i]);
+  i++;
+}
 // Super Stretch: Using forEach().
-
+var mantra2 = []
+for (let i = 0; i < mantra.length; i++) {
+  mantra2.push(mantra[i]);
+}
+mantra2.forEach(function(element) {
+  console.log(element);
+})
 // Consider the following variable:
 
 var message = "thisisateststring"
 
 // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+
+// Route #1
+function noVowelsAllowed() {
+  console.log(message.replace(/[aeiou]/gi, ""));
+}
+noVowelsAllowed();
+
+// Route #2
+var message = "thisisateststring";
+var chars = message.split("");
+var newChars = [];
+
+function noVowels(str) {
+    for (i=0; i < str.length; i++) {
+        if (str[i] !== "a" && str[i] !== "e" && str[i] !== "i" && str[i] !== "o" && str[i] !== "u" && str[i] !== "y") {
+            newChars.push(str[i]);
+        }
+    }
+    return newChars.join("");
+}
+console.log(noVowels(chars));
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
 
