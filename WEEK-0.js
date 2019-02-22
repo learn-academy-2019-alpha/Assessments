cfee6ce0e7d5c599b9112f1fd89fe538e1cd1211
Krithika Jagadeesan <krithikajagadeesan@marco-macbook.localdomain>
2019-02-22 11:12:00 -0800

    Finished Week 0

--- WEEK-0.js
+++ WEEK-0.js
@@ -1,20 +1,26 @@
 // Consider this variable:
-
 var mantra = "Be the dev";
 
-// 1. Write the code that returns 'B' from mantra.
+// 1. Write the code that returns 'B' from mantra. (I wrote this function out because it said to return...)
+function letter(num){
+ return mantra.charAt(num);
+}
+letter(0);
 
 // 2. Write the code that determines if there is a 'x' in mantra.
+mantra.includes('x')
 
 // 3. Write the code that determines if there is a 'v' in mantra.
 
-// Stretch: Write the code that returns the position of 'v' in mantra.
+mantra.includes('v')
 
+// Stretch: Write the code that returns the position of 'v' in mantra.
+mantra.indexOf('v')
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
+var codingIsEasy=true
+var imBored=true
 
 if (codingIsEasy && imBored) {
     console.log("I need more!")
@@ -26,8 +32,8 @@ if (codingIsEasy && imBored) {
 
 // Consider the following statement:
 
-var imLost
-var imFrustrated
+var imLost=false
+var imFrustrated=false
 
 if (imLost || imFrustrated) {
     console.log('Break time!')
@@ -39,22 +45,103 @@ if (imLost || imFrustrated) {
 
 // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
 
-// Stretch: Using a while loop.
+// (The for loop below will log the letters and spaces in mantra on the console)
 
-// Super Stretch: Using forEach().
+var mantra = "Be the dev";
+for (let i=0; i<mantra.length; i++){
+  console.log(mantra[i]);
+}
 
-// Consider the following variable:
+// (The for loop below will log the only the letters of mantra, but not the spaces on the console)
 
-var message = "thisisateststring"
+for (let i=0; i<mantra.length; i++){
+  var letter = /^[A-Za-z]+$/
+    if (mantra[i].match(letter)){
+    console.log(mantra[i]);
+    }
+}
+
+// Stretch: Using a while loop. (The while loop below will log the letters and spaces in mantra on the console)
+var mantra = "Be the dev";
+var i = 0;
+while (i < mantra.length) {
+  console.log(mantra[i]); i++ ;
+}
+
+// Super Stretch: Using forEach(). (The forEach() method below will log the letters and spaces in mantra on the console)
+var mantra = "Be the dev";
+var array= mantra.split("")
+array.forEach(function(element) {
+  console.log(element);
+});
+
 
 // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+var message = 'thisisateststring'
+
+var newmessage = ''
+function removeVowel(string){
+    for (let i = 0; i < string.length; i++) {
+        if (string[i]!== 'a'&& string[i]!=='e'&& string[i]!=='i'&& string[i]!=='o'&& string[i]!=='u'){
+            newmessage += string[i] ;
+        }
+    }
+      return newmessage;
+}
+removeVowel(message);
+
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
 
+var message = 'thisisateststring'
+var newmessage = ''
+function removeVowel(string){
+    if (typeof string!=='string'){
+    console.log('Not a String!');
+    } else{
+        for (let i = 0; i < string.length; i++) {
+            if (string[i]!== 'a'&& string[i]!=='e'&& string[i]!=='i'&& string[i]!=='o'&& string[i]!=='u'){
+            newmessage += string[i] ;
+              }
+          }
+      return newmessage;
+      }
+}
+removeVowel(message);
+
+
 // Super Duper Stretch: Update your function to throw a TypeError if the argument is not a string.
+var message = 'thisisateststring'
+var newmessage = ''
+function removeVowel(string){
+    if (typeof string!=='string'){
+    throw new TypeError('Not A String', WEEK-0.js, 120);
+    // The TypeError contains file name, file name (optional) and line number (optional)
+    } else{
+        for (let i = 0; i < string.length; i++) {
+            if (string[i]!== 'a'&& string[i]!=='e'&& string[i]!=='i'&& string[i]!=='o'&& string[i]!=='u'){
+            newmessage += string[i] ;
+              }
+          }
+      return newmessage;
+      }
+}
+removeVowel(message);
 
-// Consider the following variable:
-
-var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
 // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+
+var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
+var cats = []
+function onlyCats(tooncritters){
+    for (let i=0; i<tooncritters.length; i++){
+        if(tooncritters[i].animal==='cat'){
+        tooncritters[i];
+        cats.push(tooncritters[i])
+        //If you want only names in cats you can include...
+        // cats.push(tooncritters[i].name)
+        }
+    }
+    return cats;
+}
+onlyCats(toonimals);
--- WEEK-0.md
+++ WEEK-0.md
@@ -1,15 +1,21 @@
 Follow the steps in the readme of this repo, or the instructions in the curriculum to get setup with a fork of this repo.
-
 Answer these questions, then follow the instructions to submit your first assessment!
 
 #### 1. What is a function? Why would you use one?
+A function is a reusable block of code in JS that takes in a value and returns a result. If you want the same changes to happen to many values then you would pass all of
+them through the same function, this is efficient and limits redundant code.
 
 #### 2. What is a method?
+It is a property that contains a function statement. There are built in methods present in JS that can be used.
 
 #### 3. What are some examples of javascript string methods?
+Examples: .indexOf() , .search() , .charAt(), .match()
 
 #### 4. Explain the difference between accessor and mutator methods. Why would you use one over the other?
+Mutator methods return a changed or mutated form of the original value. While accessor methods result in a completely different value. If you want to change or manipulate the original value then use the mutator method. If you want  
 
 #### 5. Explain the differences between console.log() and return. Why would you use one over the other?
+console.log() prints the value inside the parentheses in the JavaScript console. We would use it for only for debugging to see if our code is working or broken, since it does not effect other code in our document. While return passes value (like from a function) from one scope to another, and this value can't be seen in the terminal unless it is stored in a variable, and console.log(variable). We use return to get value and use that value in our program.
 
 #### 6. What is one thing we did in class this week that helped you learn the most?  
+Pair programming helped me the most!I was able to interact and learn from my programming partners!
