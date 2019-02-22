bc745be8b986199dd33406aebef971e0ca938aa2
Robert Jenkins <robertjenkins@Roberts-MBP.localdomain>
2019-02-22 11:25:06 -0800

    comp week 0

--- WEEK-0.js
+++ WEEK-0.js
@@ -3,42 +3,52 @@
 var mantra = "Be the dev";
 
 // 1. Write the code that returns 'B' from mantra.
-
+console.log(mantra.charAt(0))
 // 2. Write the code that determines if there is a 'x' in mantra.
+var includesX = mantra.includes("x")
+console.log(includesX)
 
 // 3. Write the code that determines if there is a 'v' in mantra.
+var includesV = mantra.includes("v")
+console.log(includesV)
 
 // Stretch: Write the code that returns the position of 'v' in mantra.
-
+console.log(mantra.indexOf("v"))
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
+var codingIsEasy = true
+var imBored = true
 
 if (codingIsEasy && imBored) {
-    console.log("I need more!")
+   console.log("I need more!")
 } else {
-    console.log("Steady as she goes")
+   console.log("Steady as she goes")
 }
 
-// 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
+// 4. Update the codingIsEasy and imBored variables above the
+//statement to the values required to log "I need more!"
 
 // Consider the following statement:
 
-var imLost
-var imFrustrated
+var imLost = false
+var imFrustrated = false
 
 if (imLost || imFrustrated) {
-    console.log('Break time!')
+   console.log('Break time!')
 } else {
-    console.log('Keep coding!')
+   console.log('Keep coding!')
 }
 
-// 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
+// 5. Update the imLost and imFrustated variables above the
+//statement to the values required to log "Keep coding!"
 
-// 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
+// 6a. Recall the mantra variable from the first question.
+//Write the code that logs each letter of mantra using a for loop.
 
+for(let i=0; i<mantra.length; i++){
+console.log(mantra[i]);
+}
 // Stretch: Using a while loop.
 
 // Super Stretch: Using forEach().
@@ -47,7 +57,26 @@ if (imLost || imFrustrated) {
 
 var message = "thisisateststring"
 
-// 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+// 7. Write a function that takes a string like message as an argument and
+//returns the string without vowels.
+
+// function noVowels(message){
+//   return message.replace(/[aeiou]/ig, "")
+// }
+//
+// console.log(noVowels(message))
+
+function noVowels(name){
+   var word= ""
+   for(let i=0; i<name.length; i++){
+   if(name[i] !== "a" && name[i] !== "e" && name[i] !== "i" && name[i] !== "o" && name[i] !== "u"){
+   word += name[i]}
+ } return word
+}
+
+console.log(noVowels(message));
+
+
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
 
@@ -55,6 +84,23 @@ var message = "thisisateststring"
 
 // Consider the following variable:
 
-var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
+var toonimals = [ {name: 'Itchy', animal: 'mouse'},
+                 {name: 'Stimpy', animal: 'cat'},
+                 {name: 'Daffy', animal: 'duck'},
+                 {name: 'Scratchy', animal: 'cat'},
+                 {name: 'Ren', animal: 'dog'},
+                 {name: 'Felix', animal: 'cat'}]
 
-// 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+// 8. Write a function that takes an array like toonimals and returns an array
+//with only the toons that are cats.
+
+function onlyCats(animal){
+ var newVar = []
+ for(let i=0; i<animal.length; i++){
+   if(animal[i].animal === "cat"){
+     newVar.push(animal[i])
+   }
+ } return newVar
+}
+
+console.log(onlyCats(toonimals))
--- WEEK-0.md
+++ WEEK-0.md
@@ -3,13 +3,18 @@ Follow the steps in the readme of this repo, or the instructions in the curricul
 Answer these questions, then follow the instructions to submit your first assessment!
 
 #### 1. What is a function? Why would you use one?
+A function is a block of code that has a job. It can be called to perform that job or task as many times as I need it to. It can also take multiple arguments.
 
 #### 2. What is a method?
+A method is preset syntax that can help you play with your data.
 
 #### 3. What are some examples of javascript string methods?
+var sentence = "Robert is checking in"
 
+console.log(sentence.indexOf("c")) console.log(sentence.length) console.log(sentence.replace("c", "s")) console.log(sentence.toUpperCase()) console.log(sentence.toLowerCase())
 #### 4. Explain the difference between accessor and mutator methods. Why would you use one over the other?
-
+Accessor (getter) methods only let you look at data--they don't change it. Mutator (setter) methods let you change data. I would use the accessor to look up specific properties of an object. I would use Mutator methods to change information within the property.
 #### 5. Explain the differences between console.log() and return. Why would you use one over the other?
-
+console.log() only prints what you ask it to print on to the console. Return on the other hand, is like console.log() in that it will print. However, Return is used when running functions or arrays. Also, returns can only be used once within a function.
 #### 6. What is one thing we did in class this week that helped you learn the most?  
+I suppose it was when we broke off into pairs and were able to mess with the challenges.
