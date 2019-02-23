d93181da669c35def7dcb9b0fc46f17cf544f071
Tucker Mullen <tuckermullen@MarcoCaosiPhone.localdomain>
2019-02-22 16:04:54 -0800

    Tucker - Week 1 Assessments Complete

--- Week-1.js
+++ Week-1.js
@@ -1,19 +1,76 @@
 //-----------JavaScript Practical Assessment Week 1
 
-// 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three" or, print the number and "is not divisble by three".
+// 1. Write an anonymous function that takes one argument of type number and decides if that number is
+// evenly divisble by three or not. If it is, print the number and "is divisible by three" or, print the
+// number and "is not divisble by three".
 
-// 2. Write an object called helloMe. Include your first_name, last_name and at least two other properties of you. Write a function that returns a description of you.
+const anonymousFunc = (num) => {
+  if (num % 3 === 0) {
+    return `${num} is divisible by three`
+  } else {
+    return `${num} is not divisible by three`
+  }
+}
+console.log(anonymousFunc(3));
+console.log(anonymousFunc(5));
 
-// 3. Create an array of 5 grocery items. Write a function that returns the first, middle, and last item as a string.
+// 2. Write an object called helloMe. Include your first_name, last_name and at least two other properties
+// of you. Write a function that returns a description of you.
 
-// 4. Write a function called alphabetSoup that takes an argument of "str" and return a string with the letters in alphabetical order (ie. learn becomes aelnr). Assume numbers and punctuation symbols will not be included in the parameter.
+let helloMe = {
+  first_name: "Tucker",
+  last_name: "Mullen",
+  eye_color: "brown",
+  hair_color: "dark brown"
+}
+const me = () => {
+  return `My name is ${helloMe.first_name} ${helloMe.last_name}. I have ${helloMe.eye_color} eyes and ${helloMe.hair_color} hair.`
+}
+console.log(me());
 
-// 5. Given the arrays below, write a function animalNums that uses a for loop to print one value from each array concatenated together.
+// 3. Create an array of 5 grocery items. Write a function that returns the first, middle, and
+// last item as a string.
 
-var nums = [9, 5, 88, 2, 5, 42, 57]
+let groceryItems = ["cheese", "steak", "milk", "eggs", "chips"]
 
-var nouns = ["ducks", "elephants", "pangolin", "rhinoceros", "giraffes", "penguins", "llamas"]
+const specificFoods = (arr) => {
+  let first = arr[0]
+  let middle = arr[Math.floor((arr.length - 1) / 2)]
+  let last = arr.slice(-1).toString();
+  return `${first}, ${middle}, ${last}`
+}
+console.log(specificFoods(groceryItems));
+// 4. Write a function called alphabetSoup that takes an argument of "str" and return a string
+// with the letters in alphabetical order (ie. learn becomes aelnr). Assume numbers and punctuation
+// symbols will not be included in the parameter.
+
+const alphabetSoup = (str) => {
+  let arr = str.toLowerCase().split("")
+  let newArr = arr.sort().join('')
+  return newArr
+}
+console.log(alphabetSoup("Tucker"));
+// 5. Given the arrays below, write a function animalNums that uses a for loop to print one
+// value from each array concatenated together.
+
+const animalNums = () => {
+  var nums = [9, 5, 88, 2, 5, 42, 57]
+  var nouns = ["ducks", "elephants", "pangolin", "rhinoceros", "giraffes", "penguins", "llamas"]
+  var howManyAnimals = ""
+    for(let i = 0; i < nums.length; i++) {
+      howManyAnimals += nums[i] + " " + nouns[i] + " "
+    }
+    return howManyAnimals
+}
+console.log(animalNums());
 
 // output should be: "9 ducks", etc
 
-//6. Create an array of 5 numbers. Write a function that loops through the array and returns a new array of the numbers multiplied by 5.
+//6. Create an array of 5 numbers. Write a function that loops through the array and returns a new
+// array of the numbers multiplied by 5.
+
+let fiveNum = [1, 4, 7, 24, 13]
+const multByFive = (arr) => {
+  return arr.map(value => value * 5)
+}
+console.log(multByFive(fiveNum));
--- Week-1.md
+++ Week-1.md
@@ -3,24 +3,56 @@
 ### Without using Google answer the following:
 
 1. List the data types in Javascript. Indicate which are primitives.
+Number, String, Boolean, Object, Symbol, Undefined, Null, NaN
+The primitives data types are number, string, null, undefined, and boolean.
 
 2. What's the difference between =, ==, and === in JavaScript?
+A single = assigns a value to something.
+A double == compares two things and checks if they are similar.
+ex: var 41 == "41" `//truthy`
+A triple === checks if the value of something is the same value as it what it is being checked against.
 
 3. What is the difference between an array's index and length?
+An array's index is the position of an element within that array as to the length is how many elements are within
+that array.
 
 4. What are the three parts of a for loop?
+The three parts of the for loop are:
+  - 1. Telling the loop where to begin (i = 0)
+  - 2. Telling the loop when to stop looping (i < array.length)
+  - 3. Telling the loop what to do each time it loops. (i++ or i--)
 
 5. What is a function's declaration, argument, and call?
+The declaration is when you declare that the following is a function by using the term "function" before the rest
+of your code.
+
+The argument is the data that is going to be passed within the function. It's the data that the function uses
+when it operates upon being called.
+
+The call is when the function is ordered to run.
 
 6. What are the three main steps in saving work to github?
+git add .
+git commit -m "message"
+git push
 
 7. What is the terminal command to move directories?
+cd <directory name>
+cd .. (move one directory back)
 
 8. Do you have a suggestion for a Check In question?
+What is your favorite place to travel/visit?
+What is your dream job?
+What previous jobs have you had?
 
 9. What was your favorite topic this week?
+My favorite topic this week was the testing. It was quick and short but it made the programming process
+easier for me because I could write the test, then write the actually Javascript which was easier
+after writing the tests.
 
 10. What was your "A-ha!" moment this week?
+My "A-ha!" moment this week was when we were completing the object inheritance portion of the week. Objects were very
+grey to me up until then. I'm not 100% comfortable just yet but I feel much better about it now.
 
 ### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
 
@@ -36,6 +68,12 @@ function logIt(){
 logIt()
 ```
 
+The browser's console will log `undefined` because the variable `var text = 'outside'` is not available to the
+function because of closure. I believe this is an example of hoisting.
+
 2. What is JSON? How does it relate to javascript objects?
+JSON is a javascript object language used to store object data. It allows you to work with javascript objects as data
+in a less complicated way.
 
 3. Describe a closure, what is it good for and how do you recognize one?
+Closure restricts the usage of variables based on scope. Closure has access to global variables and variables declared within itself. Closure preserves the functions variable and upon the function being called, the variables located inside no longer exist. Recognizing it is where I am getting stuck.
