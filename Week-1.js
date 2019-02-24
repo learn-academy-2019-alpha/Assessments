210f9dc6be5e66c83131e3f0e42af8539273445c
ItsGabeTheBabe <certifiedbeautygabe@gmail.com>
2019-02-23 17:27:58 -0800

    just uncommented some stuff

--- Week-1.js
+++ Week-1.js
@@ -2,57 +2,57 @@
 
 // 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three" or, print the number and "is not divisble by three".
 
-// const divisbleBy3 = (num) =>{
-//     if(num % 3 === 0){
-//         return  num + " is divisble by 3"
-//     } else {
-//         return  num + ' is NOT divisble by 3'
-//     }
-// }
-//  console.log(divisbleBy3(7));
+const divisbleBy3 = (num) =>{
+    if(num % 3 === 0){
+        return  num + " is divisble by 3"
+    } else {
+        return  num + ' is NOT divisble by 3'
+    }
+}
+ console.log(divisbleBy3(7));
 // 2. Write an object called helloMe. Include your first_name, last_name and at least two other properties of you. Write a function that returns a description of you.
 //
-// const helloMe = {
-//     first_name: 'Gabriel',
-//     last_name: 'Torres',
-//     eye_color: 'Brown',
-//     age: 18
-// }
-//
-// const personInfo = (person) => {
-//     return person
-// }
-//
-// console.log(personInfo(helloMe));
+const helloMe = {
+    first_name: 'Gabriel',
+    last_name: 'Torres',
+    eye_color: 'Brown',
+    age: 18
+}
+
+const personInfo = (person) => {
+    return person
+}
+
+console.log(personInfo(helloMe));
 // 3. Create an array of 5 grocery items. Write a function that returns the first, middle, and last item as a string.
 
-// const groceryItems = ['eggs', 'chicken', 'rice', 'pasta', 'veggies']
-//
-// const fML = (array) => {
-//     var firstItem = array[0];
-//     var lastItem = array[array.length-1];
-//     var middleItem = array[Math.floor(array.length / 2)]
-//
-//      var objOutput = {
-//        first : firstItem,
-//        middle: middleItem,
-//        last : lastItem,
-//       };
-//
-//     return objOutput;
-// }
+const groceryItems = ['eggs', 'chicken', 'rice', 'pasta', 'veggies']
 
-// console.log(fML(groceryItems));
+const fML = (array) => {
+    var firstItem = array[0];
+    var lastItem = array[array.length-1];
+    var middleItem = array[Math.floor(array.length / 2)]
+
+     var objOutput = {
+       first : firstItem,
+       middle: middleItem,
+       last : lastItem,
+      };
+
+    return objOutput;
+}
+
+console.log(fML(groceryItems));
 // 4. Write a function called alphabetSoup that takes an argument of "str" and return a string with the letters in alphabetical order (ie. learn becomes aelnr). Assume numbers and punctuation symbols will not be included in the parameter.
 
 
-// const randLetters = 'bcaedf'
-//
-// const alphabetSoup = (str) => {
-//     return str.split('').sort().join('')
-//
-// }
-//  console.log(alphabetSoup(randLetters));
+const randLetters = 'bcaedf'
+
+const alphabetSoup = (str) => {
+    return str.split('').sort().join('')
+
+}
+ console.log(alphabetSoup(randLetters));
 
 
 
@@ -66,20 +66,17 @@ var nouns = ["ducks", "elephants", "pangolin", "rhinoceros", "giraffes", "pengui
 
 // My original answer
 //
-//get an index of both arrays and concat them as a new index in a new array and loop
-//push into new arry maybe?
-// const animalNums = (arr1, arr2) => {
-// var newArr = []
-// for (let i = 0; i < arr1.length, i < arr2.length; i++ ){
-//     var combo = arr1[i] + ' ' + arr2[i]
-//     newArr.push(combo)
-//     }
-//     return newArr.join()
-// }
-// console.log(animalNums(nums,nouns));
-
-//get an index of both arrays and concat them as a new index in a new array and loop
-//push into new arry maybe?
+// get an index of both arrays and concat them as a new index in a new array and loop
+// push into new arry maybe?
+const animalNums = (arr1, arr2) => {
+var newArr = []
+for (let i = 0; i < arr1.length, i < arr2.length; i++ ){
+    var combo = arr1[i] + ' ' + arr2[i]
+    newArr.push(combo)
+    }
+    return newArr.join()
+}
+console.log(animalNums(nums,nouns));
 
 //tried to clean my answer up based on Dantes answer, I made variables that
 //didn't need to be made. I think I tend to over think stuff. I kept getting
