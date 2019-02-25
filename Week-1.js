7bbd0e6c51214dbfd4173522d03fc823e3a67cf6
Robert Jenkins <robertjenkins@Roberts-MacBook-Pro.local>
2019-02-24 17:25:12 -0800

    finished week-1

--- Week-1.js
+++ Week-1.js
@@ -1,19 +1,115 @@
 //-----------JavaScript Practical Assessment Week 1
 
-// 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three" or, print the number and "is not divisble by three".
+// 1. Write an anonymous function that takes one argument of type number and
+//decides if that number is evenly divisble by three or not. If it is, print
+//the number and "is divisible by three" or, print the number
+//and "is not divisble by three".
 
-// 2. Write an object called helloMe. Include your first_name, last_name and at least two other properties of you. Write a function that returns a description of you.
+// function divisibleBy(num1){
+//   if(num1 % 3 === 0){
+//     return num1 + " is divisible by three"
+//   } else {
+//     return num1 + " is not divisible by three"
+//   }
+// }
+//
+// console.log(divisibleBy(5))
+// console.log(divisibleBy(6))
 
-// 3. Create an array of 5 grocery items. Write a function that returns the first, middle, and last item as a string.
 
-// 4. Write a function called alphabetSoup that takes an argument of "str" and return a string with the letters in alphabetical order (ie. learn becomes aelnr). Assume numbers and punctuation symbols will not be included in the parameter.
+// 2. Write an object called helloMe. Include your first_name, last_name and
+//at least two other properties of you. Write a function that returns a
+//description of you.
 
-// 5. Given the arrays below, write a function animalNums that uses a for loop to print one value from each array concatenated together.
+// var helloMe = {
+//   firstName: "Robert",
+//   lastName: "Jenkins",
+//   shoeSize: 11,
+//   phoneNumber: 6199050564,
+// }
+//
+// function robert(){
+//   return helloMe
+// }
+//
+// console.log(robert())
 
-var nums = [9, 5, 88, 2, 5, 42, 57]
+// 3. Create an array of 5 grocery items. Write a function that returns the
+//first, middle, and last item as a string.
 
-var nouns = ["ducks", "elephants", "pangolin", "rhinoceros", "giraffes", "penguins", "llamas"]
+// var groceryItems = ["lettuce", "tomato", "onion", "avocado", "buns"]
+//
+// var odds =[]
+//
+// const oddItemsOnly = (array) => {
+//   for(let i=0; i < array.length; i++){
+//     if(i % 2 === 0){
+//       odds.push(array[i])
+//     }
+//   } return odds.toString()
+// }
+// console.log(oddItemsOnly(groceryItems))
 
+//refactoring
+
+// const shoppingBag = (array) => {
+//   let cart = array.filter((value, index) => {
+//     return index % 2 === 0
+//   })
+//   return cart.join(" ")
+// }
+// console.log(shoppingBag(groceryItems));
+
+
+// 4. Write a function called alphabetSoup that takes an argument of "str" and
+//return a string with the letters in alphabetical order (ie. learn becomes aelnr).
+// Assume numbers and punctuation symbols will not be included in the parameter.
+
+// const alphabetSoup = (str) => {
+//   return str.split("").sort().join("")
+// }
+// console.log(alphabetSoup("learn"));
+//
+
+
+
+// 5. Given the arrays below, write a function animalNums that uses a for
+//loop to print one value from each array concatenated together.
+
+// var nums = [9, 5, 88, 2, 5, 42, 57]
+//
+// var nouns = ["ducks", "elephants", "pangolin", "rhinoceros", "giraffes", "penguins", "llamas"]
+
+// const animalNums = (array) => {
+//   // new variable to store nums and nouns return
+//   let numNouns = []
+//   //use a for loop
+//   for(let i=0; i < array.length; i++){
+//     numNouns += nums[i] + " " + nouns[i] + " "
+//   } return numNouns
+// }
+//
+// console.log(animalNums(nums))
+
+// attempted refactoring
+// const animalNums2 = (array1, array2) => {
+//
+//   let joined = array1.map((value,index) => value + array2[index])
+//   return joined.join(" ")
+// }
+//
+//
+// console.log(animalNums2(nums, nouns));
 // output should be: "9 ducks", etc
 
-//6. Create an array of 5 numbers. Write a function that loops through the array and returns a new array of the numbers multiplied by 5.
+//6. Create an array of 5 numbers. Write a function that loops through
+//the array and returns a new array of the numbers multiplied by 5.
+
+// var myNums = [1, 2, 3, 4, 5]
+//
+// const mult5 = (array) => {
+//   let newArr = array.map(value => value * 5)
+//   return newArr
+// }
+//
+// console.log(mult5(myNums))
--- Week-1.md
+++ Week-1.md
@@ -3,24 +3,40 @@
 ### Without using Google answer the following:
 
 1. List the data types in Javascript. Indicate which are primitives.
+There are seven data types. Primitive data types = [number, strings, boolean] composite data types= [objects, functions, arrays] trivial data types =[null and undefined]
 
 2. What's the difference between =, ==, and === in JavaScript?
+(=) would be used to assign something to a var. (==) would be used in the same way but its preferred to use this if you want more flexibility with data types. For example a string of "seven" vs 7.
 
 3. What is the difference between an array's index and length?
+An index of an array is the specific placement and the length is how long that index is.
 
 4. What are the three parts of a for loop?
+The three parts are- The initializer, the condition and the final expression.
+example-
+for ([initialization]; [condition]; [final-expression]){
+
+}
 
 5. What is a function's declaration, argument, and call?
+A declaration is the name of the variable. The argument is where the placeholder is. And when you call it you use the functions name followed by parenthesis.
 
 6. What are the three main steps in saving work to github?
 
+git add
+git commit -m
+git push
+
 7. What is the terminal command to move directories?
+  The terminal command is "cd".
 
 8. Do you have a suggestion for a Check In question?
+  What are some hobbies you have? or what hobbies do you have that have a major impact on your wellbeing?
 
 9. What was your favorite topic this week?
+ Testing was a fav for this week.
 
-10. What was your "A-ha!" moment this week?
+10. What was your "A-ha!" moment this week? When I compared global scope to weird gamer nerd things I can relate to.
 
 ### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
 
@@ -31,11 +47,15 @@ var text = 'outside'
 function logIt(){
     console.log(text)
     var text = 'inside'
+
 }
 
 logIt()
 ```
+//answer- The console shows "undefined" because "console.log" within the function logIt gives the function closure. We then created var text = "inside" but that was after we logged it. When we called it below, it wouldnt access the var = "outside" because that variable lives outside the function being called. I may be entirely wrong on this.
 
 2. What is JSON? How does it relate to javascript objects?
+JSON stands for JavaScript Object Notation. I had to google it... but... I think it relates to the syntax... I had a hard time trying to figure this out.
 
 3. Describe a closure, what is it good for and how do you recognize one?
+A closure is what you would need to prevent a scope within a function messing with global scope and vice versa. Im not sure how you would recognize one... I suppose within the code on stretch question-1 There was closure with the brackets...
