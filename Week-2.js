1f616742338e3449e7f6882b12497597c1ba693d
ItsGabeTheBabe <certifiedbeautygabe@gmail.com>
2019-03-03 18:10:56 -0800

    Finished week-2js assessments besides question 4

--- Week-2.js
+++ Week-2.js
@@ -2,34 +2,53 @@
 
 // 1. Write an arrow function that takes one argument of type array and returns the contents of that array in reverse order.
 
-var originalArray = [1, 2, 3, 4, 5, 6, 7]
+// var originalArray = [1, 2, 3, 4, 5, 6, 7]
+//
+// const reverse = (array) => {
+// 	return array.reverse()
+// }
+//
+// console.log(reverse(originalArray));
 
 
 // 2. Given the object below, complete the console.log to find specific information:
 
-var bicycle = {
-	type: "Roadbike",
-	gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
-	wheels: {
-		count: 2,
-		specs: ["road tires", "AX-7563", "80-115 PSI"],
-		brand: "Trek"
-	}
-}
-
-// Log the type of bicycle:
-console.log()
-
-// Log the bell:
-console.log()
-
-// Log the PSI:
-console.log()
+// var bicycle = {
+// 	type: "Roadbike",
+// 	gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
+// 	wheels: {
+// 		count: 2,
+// 		specs: ["road tires", "AX-7563", "80-115 PSI"],
+// 		brand: "Trek"
+// 	}
+// }
+//
+// // Log the type of bicycle:
+// console.log(bicycle.type)
+//
+// // Log the bell:
+// console.log(bicycle.gear[2])
+//
+// // Log the PSI:
+// console.log(bicycle.wheels.specs[2])
 
 
 // 3. Write a function called letterCounter that logs the number of times the letter "l" appears in ourString.
 
-var ourString = "Hello Learn Students!"
+// var ourString = "Hello Learn Students!"
+//
+// const letterCounter = (string, letter) => {
+// const newStr =	string.toLowerCase()
+// 	var counter = 0
+// 	for (let i = 0; i < newStr.length; i++) {
+// 		if(newStr.charAt(i) == letter){
+// 			counter += 1;
+// 		}
+// 	}
+// 	return counter
+// }
+//
+// console.log(letterCounter(ourString, 'l'));
 
 
 // 4. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
@@ -37,4 +56,18 @@ var ourString = "Hello Learn Students!"
 
 //5. Write a function called oddChecker that takes in the fullArr and uses a for loop to return a new array of only odd numbers. Copy and paste your code and refactor using ES6 syntax.
 
-var fullArr = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+// var fullArr = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+//
+// const oddChecker = (array) => {
+//
+// 	const newArr = []
+//
+// for (let i = 0; i < array.length; i++) {
+// 		if(array[i] % 2 !== 0 && typeof array[i] !== 'string' && typeof array[i] !== 'boolean'){
+// 			 newArr.push(array[i])
+// 		}
+// 	}
+// 	return newArr
+// }
+//
+// console.log(oddChecker(fullArr));
